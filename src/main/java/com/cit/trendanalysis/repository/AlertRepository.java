ff9ee6734d336ba6d95ac5737032dd80396f644a
eamon.foy <eamon.foy@mycit.ie>
2018-12-15 18:59:18 +0000

    Update AlertRepository.java

--- src/main/java/com/cit/trendanalysis/repository/AlertRepository.java
+++ src/main/java/com/cit/trendanalysis/repository/AlertRepository.java
@@ -7,11 +7,16 @@ import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.geo.GeoPoint;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.UUID;
 
 @Slf4j
@@ -31,43 +36,47 @@ public class AlertRepository {
 
     }
 
+
+
+
+
+
     public void insertAlert(Alert alert) throws IOException {
 
         alert.setId(UUID.randomUUID().toString());
 
+
+
+
         XContentBuilder builder = XContentFactory.jsonBuilder()
                 .startObject()
-                .field("severity", alert.getSeverity())
-                .field("title", alert.getTitle())
-                .field("description", alert.getDescription())
-                .startObject("currentEvent")
-                .field("timestamp", alert.getCurrentEvent().getTimestamp())
-                .field("cardId", alert.getCurrentEvent().getCardId())
-                .field("panelId", alert.getCurrentEvent().getPanelId())
-                .startObject("location")
-                .startObject("coordinates")
-                .field("lat", alert.getCurrentEvent().getLocation().getCoordinates().getLatitude())
-                .field("lon", alert.getCurrentEvent().getLocation().getCoordinates().getLongitude())
-                .endObject()
-                .field("altitude", alert.getCurrentEvent().getLocation().getAltitude())
-                .field("relativeLocation", alert.getCurrentEvent().getLocation().getRelativeLocation())
-                .endObject()
-                .field("accessAllowed", alert.getCurrentEvent().isAccessAllowed())
-                .endObject()
-                .startObject("previousEvent")
-                .field("timestamp", alert.getPreviousEvent().getTimestamp())
-                .field("cardId", alert.getPreviousEvent().getCardId())
-                .field("panelId", alert.getPreviousEvent().getPanelId())
-                .startObject("location")
-                .startObject("coordinates")
-                .field("lat", alert.getPreviousEvent().getLocation().getCoordinates().getLatitude())
-                .field("lon", alert.getPreviousEvent().getLocation().getCoordinates().getLongitude())
-                .endObject()
-                .field("altitude", alert.getPreviousEvent().getLocation().getAltitude())
-                .field("relativeLocation", alert.getPreviousEvent().getLocation().getRelativeLocation())
-                .endObject()
-                .field("accessAllowed", alert.getPreviousEvent().isAccessAllowed())
-                .endObject()
+                    .field("severity", alert.getSeverity())
+                    .field("title", alert.getTitle())
+                    .field("description", alert.getDescription())
+                    .startObject("currentEvent")
+                        .field("timestamp", new Date(alert.getCurrentEvent().getTimestamp()))
+                        .field("cardId", alert.getCurrentEvent().getCardId())
+                        .field("panelId", alert.getCurrentEvent().getPanelId())
+                        .startObject("location")
+                           .field("coordinates", new GeoPoint(alert.getCurrentEvent().getLocation().getCoordinates().getLatitude(),
+                                   alert.getCurrentEvent().getLocation().getCoordinates().getLongitude()))
+                           .field("altitude", alert.getCurrentEvent().getLocation().getAltitude())
+                           .field("relativeLocation", alert.getCurrentEvent().getLocation().getRelativeLocation())
+                        .endObject()
+                        .field("accessAllowed", alert.getCurrentEvent().isAccessAllowed())
+                    .endObject()
+                    .startObject("previousEvent")
+                        .field("timestamp", new Date(alert.getPreviousEvent().getTimestamp()))
+                        .field("cardId", alert.getPreviousEvent().getCardId())
+                        .field("panelId", alert.getPreviousEvent().getPanelId())
+                        .startObject("location")
+                            .field("coordinates", new GeoPoint(alert.getPreviousEvent().getLocation().getCoordinates().getLatitude(),
+                                    alert.getPreviousEvent().getLocation().getCoordinates().getLongitude()))
+                            .field("altitude", alert.getPreviousEvent().getLocation().getAltitude())
+                            .field("relativeLocation", alert.getPreviousEvent().getLocation().getRelativeLocation())
+                        .endObject()
+                        .field("accessAllowed", alert.getPreviousEvent().isAccessAllowed())
+                    .endObject()
                 .endObject();
 
         try {
